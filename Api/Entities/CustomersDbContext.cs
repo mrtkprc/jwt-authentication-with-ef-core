af1670133742dd08112b36476670ec5366d0137f
Mert Koprucu <mert.koprucu@siemens.com>
2021-10-03 14:23:09 +0300

    Add DbContext

---
+++ Api/Entities/CustomersDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Entities
+{
+    public class CustomersDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public CustomersDbContext(DbContextOptions<CustomersDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+        }
+    }
+}
