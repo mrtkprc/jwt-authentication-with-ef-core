c9d5573371b6ad862de9ec576f7b1f43dab7b626
Mert Koprucu <mert.koprucu@siemens.com>
2021-10-03 15:09:51 +0300

    Add customer services.

---
+++ Api/Interfaces/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using Api.Requests;
+using Api.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<LoginResponse> Login(LoginRequest loginRequest);
+    }
+}
---
+++ Api/Services/CustomerService .cs
@@ -0,0 +1,41 @@
+﻿using Api.Entities;
+using Api.Helpers;
+using Api.Interfaces;
+using Api.Requests;
+using Api.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly CustomersDbContext customersDbContext;
+        public CustomerService(CustomersDbContext customersDbContext)
+        {
+            this.customersDbContext = customersDbContext;
+        }
+
+        public async Task<LoginResponse> Login(LoginRequest loginRequest)
+        {
+            var customer = customersDbContext.Customers.SingleOrDefault(customer => customer.Active && customer.Username == loginRequest.Username);
+
+            if (customer == null)
+            {
+                return null;
+            }
+            var passwordHash = HashingHelper.HashUsingPbkdf2(loginRequest.Password, customer.PasswordSalt);
+
+            if (customer.Password != passwordHash)
+            {
+                return null;
+            }
+
+            var token = await Task.Run(() => TokenHelper.GenerateToken(customer));
+
+            return new LoginResponse { Username = customer.Username, FirstName = customer.FirstName, LastName = customer.LastName, Token = token };
+        }
+    }
+}
