fddaa40f65c358591edc0ed1cc7272738268a36d
Mert Koprucu <mert.koprucu@siemens.com>
2021-10-03 15:10:06 +0300

    Add CustomersController

---
+++ Api/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using Api.Interfaces;
+using Api.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        {
+            if (loginRequest == null || string.IsNullOrEmpty(loginRequest.Username) || string.IsNullOrEmpty(loginRequest.Password))
+            {
+                return BadRequest("Missing login details");
+            }
+
+            var loginResponse = await _customerService.Login(loginRequest);
+
+            if (loginResponse == null)
+            {
+                return BadRequest("Invalid credentials");
+            }
+
+            return Ok(loginResponse);
+        }
+    }
+}
